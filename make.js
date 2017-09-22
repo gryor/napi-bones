3c53c33629212830911873d649738097facf7d02
Wilhelm Matilainen <wilhelm.matilainen@gmail.com>
2017-09-22 09:33:54 +0300

    build system updated

--- make.js
+++ make.js
@@ -5,14 +5,9 @@ const pkg = require('./package.json');
 const log = debug('napi-bones-make');
 
 (async function () {
-	try {
-		pkg.build = pkg.build || {};
-		Object.assign(pkg.build, {
-			name: pkg.name,
-			version: pkg.version
-		});
-		await wilu(pkg.build);
-	} catch(e) {
-		log(e);
-	}
+  try {
+    await wilu(pkg);
+  } catch(e) {
+    log(e);
+  }
 })();
